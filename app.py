c6da6e3448bb2b2b76267cd682df533dd3750a36
angelwshotgun <hoangtuanvu17082002@gmail.com>
2024-10-25 10:49:48 +0700

    zz

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask, render_template, request, make_response
 import pdfkit
+
 app = Flask(__name__)
 
-# Cấu hình wkhtmltopdf (bỏ qua nếu không dùng ở preview)
-path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'  # Đường dẫn chính xác
+path_to_wkhtmltopdf = os.path.join(os.getcwd(), 'usr', 'local', 'bin', 'wkhtmltopdf')
 config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
 
 @app.route('/')
@@ -48,32 +49,6 @@ def create_pdf():
     response.headers['Content-Disposition'] = 'attachment; filename=confirmation.pdf'
 
     return response
-    # Nhận dữ liệu từ form HTML
-    name = request.form.get('name')
-    guests = request.form.get('guests')
-    stay_dates = request.form.get('stay_dates')
-    room_type = request.form.get('room_type')
-    room_price = request.form.get('room_price')
-    total_payment = request.form.get('total_payment')
-    services_included = request.form.get('services_included')
-    other_services = request.form.get('other_services')
-    payment_note = request.form.get('payment_note')
-    payment_info = request.form.get('payment_info')
-
-    # Render HTML mà không tạo file PDF
-    return render_template(
-        'pdf_template.html',
-        name=name,
-        guests=guests,
-        stay_dates=stay_dates,
-        room_type=room_type,
-        room_price=room_price,
-        total_payment=total_payment,
-        services_included=services_included,
-        other_services=other_services,
-        payment_note=payment_note,
-        payment_info=payment_info
-    )
 
 if __name__ == '__main__':
     app.run(debug=True)
